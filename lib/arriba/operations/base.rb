d10024d24fb82d216b24fe75efc5078fd2e2f91a
Mark J. Titorenko <mark.titorenko@alces-software.com>
2011-10-06 00:28:35 +0100

    refactored volume structure - file specific operations moved out to a mixin

---
+++ lib/arriba/operations/base.rb
@@ -0,0 +1,65 @@
+require 'active_support/core_ext/object/inclusion'
+
+module Arriba
+  module Operations
+    module Base
+      attr_accessor :name, :root
+      private :name=, :root=, :name, :root
+
+      # module that mix in must define: #entries(path) and #tree(path)
+      # in order to support #files, #ls and #parents.
+      
+      def files(path)
+        [cwd(path)] + entries(path).map {|f| file(path,f) }
+      rescue Errno::EACCES
+        [cwd(path)]
+      end
+
+      def ls(path)
+        entries(path)
+      end
+
+      def parents(path)
+        # path is a directory
+        # find siblings of path and siblings of parents
+        parent = ::File.dirname(path)
+        tree(parent).tap { |d| d += parents(parent) unless parent.in?(['.','/']) }
+      end
+      
+      def cwd(path)
+        if path.in?(['.','/'])
+          base
+        else
+          represent(path)
+        end
+      end
+
+      def file(path,f = nil)
+        relative_path = f.nil? ? path : ::File.join(path,f)
+        represent(relative_path)
+      end
+
+      def locked?(path)
+        false
+      end
+
+      protected
+      def abs(*args)
+        ::File.join(root,*args)
+      end
+
+      private
+      def volume
+        self
+      end
+
+      def represent(volume, path)
+        Arriba::File.new(volume, path)
+      end
+
+      def base
+        Arriba::Root.new(volume,name)
+      end
+    end
+  end
+end
--- lib/arriba/volume.rb
+++ lib/arriba/volume.rb
@@ -1,4 +1,12 @@
 module Arriba
   class Volume
+    attr_accessor :id
+    def initialize(id)
+      self.id = id
+    end
+
+    def hash_for(path)
+      "#{self.id}_#{Arriba::Routing::encode(path)}"
+    end
   end
 end
