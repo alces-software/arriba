68d75236097a16482c1f7665e4a788147b72b0c3
Mark J. Titorenko <mark.titorenko@alces-software.com>
2011-10-14 20:33:21 +0100

    rationalization of requires

--- lib/arriba.rb
+++ lib/arriba.rb
@@ -10,9 +10,9 @@ require 'arriba/operations/base'
 require 'arriba/operations/file'
 require 'arriba/volume'
 require 'arriba/volume/directory'
-# XXX - move these out to an extension gem
-require 'arriba/volume/irods'
-require 'arriba/volume/polymorph_directory'
+require 'arriba/target'
+require 'arriba/target/base'
+require 'arriba/target/local'
 
 module Arriba
   module Base
@@ -29,21 +29,4 @@ module Arriba
     end
   end
   extend Arriba::Base
-end
-
-if Object.const_defined?(:ARRIBA_PATH)
-  load File.join(ARRIBA_PATH,'arriba','api','structures.rb')
-  load File.join(ARRIBA_PATH,'arriba','api','management.rb')
-  load File.join(ARRIBA_PATH,'arriba','api','creation.rb')
-  load File.join(ARRIBA_PATH,'arriba','api','deletion.rb')
-  load File.join(ARRIBA_PATH,'arriba','api','duplication.rb')
-  load File.join(ARRIBA_PATH,'arriba','api','base.rb')
-  load File.join(ARRIBA_PATH,'arriba','api.rb')
-  load File.join(ARRIBA_PATH,'arriba','routing.rb')
-  load File.join(ARRIBA_PATH,'arriba','mime_type.rb')
-  load File.join(ARRIBA_PATH,'arriba','file.rb')
-  load File.join(ARRIBA_PATH,'arriba','file_response.rb')
-  load File.join(ARRIBA_PATH,'arriba','root.rb')
-  load File.join(ARRIBA_PATH,'arriba','volume.rb')
-  load File.join(ARRIBA_PATH,'arriba','volume','directory.rb')
 end
