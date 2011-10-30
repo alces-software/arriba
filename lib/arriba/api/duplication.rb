ebc70f05f233d38533b81465891c4ee2b927e2d0
Mark J. Titorenko <mark.titorenko@alces-software.com>
2011-10-30 12:30:47 +0000

    robustness for (though limitation on) copy and move operations

--- lib/arriba/api/duplication.rb
+++ lib/arriba/api/duplication.rb
@@ -15,17 +15,25 @@ module Arriba
       private
       def copy
         each_target_with_destination(:added) do |src_vol, src_path, dest_vol, dest_path|
-          src_vol.copy(src_path,dest_vol,dest_path)
-          dest_vol.file(dest_path, dest_vol.name_for(src_path))
+          if src_vol == dest_vol
+            src_vol.copy(src_path,dest_path)
+            dest_vol.file(dest_path, dest_vol.name_for(src_path))
+          else
+            raise "Unable to copy across volumes"
+          end
         end
       end
 
       def cut
         removed = []
         each_target_with_destination(:added) do |src_vol, src_path, dest_vol, dest_path|
-          src_vol.move(src_path,dest_vol,dest_path)
-          removed << src_vol.hash_for(src_path)
-          dest_vol.file(dest_path,dest_vol.name_for(src_path))
+          if src_vol == dest_vol
+            src_vol.move(src_path,dest_path)
+            removed << src_vol.hash_for(src_path)
+            dest_vol.file(dest_path,dest_vol.name_for(src_path))
+          else
+            raise "Unable to move across volumes"
+          end
         end.tap do |data|
           data[:removed] = removed if removed.any?
         end    
