805d56fc880486a1e34f6067effd49a9594bbcc4
James Donohue <james.donohue@alces-software.com>
2013-11-08 20:07:47 +0000

    Remove tracing and remnants of old approach

--- lib/arriba/api/base.rb
+++ lib/arriba/api/base.rb
@@ -11,9 +11,6 @@ module Arriba
       attr_accessor :volumes, :params
 
       def initialize(volumes, params)
-        STDERR.puts "API initialize called (#{volumes.size} volumes)"
-        # Tell each volume about others to faciliate symlink resolution
-        volumes.each { |v| STDERR.puts "Setting all_vols for #{v}"; v.all_volumes = volumes }
         self.volumes = volumes
         self.params = params
       end
@@ -35,7 +32,6 @@ module Arriba
       # def resize
 
       def target(cmd = nil)
-        STDERR.puts "target cmd = #{cmd}"
         volume, path = if params[:target].present?
                          v, p = Arriba::Routing::route(params[:target])
                          # Prevent requests for '.' and '..' breaking elfinder client
